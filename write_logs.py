246aefea8947bbda0e3666f111f9db374d9962ac
chenyiz_cpen <chenyizheng97@gmail.com>
2021-03-25 12:23:29 -0700

    Restructured the functions

--- write_logs.py
+++ write_logs.py
@@ -1,10 +1,14 @@
-import os
-from datetime import datetime
+import torch
+import time
+
 
 def write_logs(line):
-    logs_date = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
-    #print(logs_date)
-    save_txt = 1
-    if save_txt:  # Write to file
-        with open(logs_date + '.txt', 'a') as f:
-            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+    with open('logs' + '.txt', 'a') as f:
+        f.write(line + '\n')
+
+
+def time_synchronized():
+    # pytorch-accurate time
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    return time.time()
