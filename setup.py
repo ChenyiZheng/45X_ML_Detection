dc67ca5c33f470c9a3fc422c794883f5ca1753d2
Henry <imhenrysitu@gmail.com>
2021-04-13 14:41:09 -0700

    Created a setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Arduino Stepper',
+    version=1.0,
+    packages=find_packages(),
+    url='https://github.com/ChenyiZheng/45X_ML_Detection',
+    author='Henry Situ, Chenyi Zheng, MECH 45X Team 10',
+    description='The detection project used to detect fire, smoke and heat sources from a thermal and visual feed.',
+    install_requires=[
+        'https://github.com/ultralytics/yolov5',
+    ],
+)
